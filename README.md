232e46aac25afa6b0c728c1a273c64406e2d040d
Faruk <felixfranko66@gmail.com>
2020-12-12 18:47:17 +0300

    added crypt password

---
+++ README.md
@@ -0,0 +1,15 @@
+MongoDB-Express-React-Node.js bookstore project.
+
+User register/sign-in/authantication
+
+  // TODO1: Validate the fields
+  // TODO2: Check already registered
+  // TODO3: Crpyt password
+  // TODO4: Save the user to DB
+
+
+
+
+
+
+MONGODB_URI=
--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,22 +1,32 @@
-exports.authRegister = (req, res) => {
-    const { firstName, lastName, email, password } = req.body;
-    console.log(
-      "Fields:",
-      firstName,
-      lastName,
-      email,
-      password
-    );
-    // TODO1: Validate the fields
-    // TODO2: check already registered
-    // TODO3: crpyt password
-    // TODO4: save the user to DB
-  
-    res.send("Register Completed.");
-  };
-  
-  exports.authLogin = (req, res) => {
-    // TODO: Auth.
-    // TODO: Login func.
-    res.send("Login Completed");
-  };
+const User = require("../models/UserModel");
+const bcrypt = require("bcryptjs");
+
+exports.authRegister = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  // TODO1: Validate the fields
+  // TODO2: check already registered
+  // TODO3: crpyt password
+  // TODO4: save the user to DB
+
+  const salt = await bcrypt.genSalt(10);
+  const newPassword = await bcrypt.hash(password, salt);
+
+  const user = new User({
+    firstName,
+    lastName,
+    email,
+    password: newPassword,
+  });
+
+  await user.save();
+
+  //TODO: Error handling for saving
+  res.send("Register Completed.");
+};
+
+exports.authLogin = (req, res) => {
+  // TODO: Auth.
+  // TODO: Login func.
+  res.send("Login Completed");
+};
