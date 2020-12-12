caa6a46980a13516382c2c193439b4daf0608679
Faruk <felixfranko66@gmail.com>
2020-12-12 18:52:26 +0300

    Added user register email check

--- models/connectDB.js
+++ models/connectDB.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-   try {
+  try {
     await mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
     });
     console.log("Successfully connected to DB");
-} catch (error) {
-    console.log("Error connecting to DB", error);
-    }
+  } catch (error) {
+    console.log("Error connecting DB", error);
+  }
 };
 
 module.exports = connectDB;
