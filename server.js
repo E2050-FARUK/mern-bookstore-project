e18f5048af740db245ee18d75a229849504bf589
Faruk <felixfranko66@gmail.com>
2020-12-13 18:04:55 +0300

    added book detail

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 require("dotenv").config();
 
 const connectDB = require("./models/connectDB");
-
 const router = require("./routes/router");
 
 connectDB();
@@ -12,6 +11,7 @@ connectDB();
 app.use(express.json());
 app.use("/api", router);
 
-app.listen(process.env.PORT, () => {
-  console.log(`I'm listening on port ${process.env.PORT}`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`I'm listening on port ${port}`);
 });
